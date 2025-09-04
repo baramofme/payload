c8ac01237f3447b574aba7ea1f4dbf21505051a6
Jihoon Yi <44565599+baramofme@users.noreply.github.com>
2025-09-04 17:42:44 +0900

    Update next.config.js

--- templates/website/next.config.js
+++ templates/website/next.config.js
@@ -1,5 +1,9 @@
+import path from 'path';
+import { fileURLToPath } from 'url';
 import { withPayload } from '@payloadcms/next/withPayload'
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 import redirects from './redirects.js'
 
 const NEXT_PUBLIC_SERVER_URL = process.env.VERCEL_PROJECT_PRODUCTION_URL
@@ -10,7 +14,9 @@ const NEXT_PUBLIC_SERVER_URL = process.env.VERCEL_PROJECT_PRODUCTION_URL
 const nextConfig = {
   images: {
     remotePatterns: [
-      ...[NEXT_PUBLIC_SERVER_URL /* 'https://example.com' */].map((item) => {
+      ...[NEXT_PUBLIC_SERVER_URL ,/* 'https://example.com' */
+      'http://192.168.45.181:4000', /* 새로운 URL을 직접 추가 */
+      ].map((item) => {
         const url = new URL(item)
 
         return {
@@ -20,7 +26,21 @@ const nextConfig = {
       }),
     ],
   },
-  webpack: (webpackConfig) => {
+  webpack: (webpackConfig, {isServer}) => {
+    if (!isServer) {
+       webpackConfig.cache = {
+          type: 'filesystem',
+          cacheDirectory: path.resolve(__dirname, './node_modules/.next/cache'),
+       }
+    }
+    webpackConfig.stats = {
+      reasons: true,
+      chunks: true,
+      modules: true,
+      source: true,
+      assets: true
+    }
+
     webpackConfig.resolve.extensionAlias = {
       '.cjs': ['.cts', '.cjs'],
       '.js': ['.ts', '.tsx', '.js', '.jsx'],
