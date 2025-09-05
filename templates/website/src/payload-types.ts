178645e4e75d0fe7d18fd24c7db790697bbbef89
Jihoon Yi <44565599+baramofme@users.noreply.github.com>
2025-09-06 03:31:42 +0900

    Update payload-types.ts

--- templates/website/src/payload-types.ts
+++ templates/website/src/payload-types.ts
@@ -108,7 +108,7 @@ export interface Config {
     header: HeaderSelect<false> | HeaderSelect<true>;
     footer: FooterSelect<false> | FooterSelect<true>;
   };
-  locale: null;
+  locale: 'ko' | 'en' | 'ja' | 'cn';
   user: User & {
     collection: 'users';
   };
